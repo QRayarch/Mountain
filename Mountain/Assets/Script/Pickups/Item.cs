296851571202fa230fc4e6e4d2ac10c752398874
unknown <afl4947@OH1380-15.main.ad.rit.edu>
2015-11-20 08:52:07 -0500

    Added a test scene and started working on the stick to push a boulder.

---
+++ Mountain/Assets/Script/Pickups/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	protected Transform trans;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		trans = transform;
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		CheckInputs();
+	}
+
+	protected virtual void CheckInputs() {
+
+	}
+}
---
+++ Mountain/Assets/Script/Pickups/Stick.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stick : Item {
+
+	public float range = 0.3f;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+	}
+
+	protected override void CheckInputs() {
+		if(Input.GetButtonDown("Fire")) {
+			Hit();
+		}
+	}
+
+	private void Hit() {
+		RaycastHit hit = new RaycastHit();
+		if(Physics.Raycast(trans.position, trans.forward, out hit, range)) {
+			GameObject objectHit = hit.collider.gameObject;
+			if(objectHit.CompareTag("Rock")) {
+				Rigidbody body = objectHit.GetComponent<Rigidbody>();
+				if(body != null) {
+					body.isKinematic = false;
+					float force = 2000;
+					body.AddForceAtPosition(trans.right * -force, hit.point);
+				}
+			}
+		}
+	}
+}
