2135c2abe58da5c37788b2199e920dee6678fc49
unknown <afl4947@GDD-29.main.ad.rit.edu>
2015-11-21 13:35:53 -0500

    Worked on the fireflies producing a more natural motion.

--- Mountain/Assets/Script/Enviormental/Firefly.cs
+++ Mountain/Assets/Script/Enviormental/Firefly.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class Firefly : MonoBehaviour {
 
+	public float chnageHeadingTime = 0.3f;
+	[Range(0, 1)]
+	public float newHeadingChance = 0.1f;
+	public float speed = 0.2f;
+	public float minGroundDistance = 0.4f;
+	public float maxGroundDistance = 1.0f;
+
+	private Vector3 newHeading;
+	private Vector3 heading;
+	private float changeHeadingTimer = 0;
+	private Transform trans;
+
 	// Use this for initialization
 	void Start () {
-		
+		heading = Random.insideUnitSphere;
+		newHeading = Random.insideUnitSphere;
+		trans = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(changeHeadingTimer >= chnageHeadingTime) {
+			trans.position += newHeading * speed * Time.deltaTime;
+		} else {
+			trans.position += Vector3.Lerp(heading, newHeading, changeHeadingTimer / chnageHeadingTime) * speed * Time.deltaTime;
+		}
+
+		float heightAboverTerrain = trans.position.y - Terrain.activeTerrain.SampleHeight(trans.position);
+		if(heightAboverTerrain < minGroundDistance) {
+			newHeading.y = Mathf.Abs(newHeading.y);
+			changeHeadingTimer = 0;
+		}
+		if(heightAboverTerrain > maxGroundDistance) {
+			newHeading.y = -Mathf.Abs(newHeading.y);
+			changeHeadingTimer = 0;
+		}
+
+		changeHeadingTimer += Time.deltaTime;
+		if(Random.Range(0, 1.0f) <= newHeadingChance) { 
+			changeHeadingTimer = 0;
+			heading = newHeading;
+			newHeading = Random.insideUnitSphere;
+		}
 	}
 }
