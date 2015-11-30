070c046217763e8264c086860ffb1d246518730d
unknown <afl4947@OH1380-15.main.ad.rit.edu>
2015-11-30 08:50:33 -0500

    Worked on the travel script.

---
+++ Mountain/Assets/Script/Transversal/Door.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public Transform travelLocation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.CompareTag("Player")) {
+			other.transform.position = travelLocation.position;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(transform.position, transform.localScale);
+		if(travelLocation != null) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireCube(travelLocation.position, travelLocation.localScale);
+		}
+	}
+}
